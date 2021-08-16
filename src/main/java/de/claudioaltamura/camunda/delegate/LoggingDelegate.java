da56d16315df440fb237fd92d10eaeb6c73cde3f
Claudio Altamura <claudio@claudioaltamura.de>
2021-08-16 19:35:21 +0200

    chg: removed throwing clause

--- src/main/java/de/claudioaltamura/camunda/delegate/LoggingDelegate.java
+++ src/main/java/de/claudioaltamura/camunda/delegate/LoggingDelegate.java
@@ -10,7 +10,7 @@ public class LoggingDelegate implements JavaDelegate {
 	private static final Logger LOGGER = Logger.getLogger(LoggingDelegate.class.getName());
 
 	@Override
-	public void execute(DelegateExecution delegateExecution) throws Exception {
+	public void execute(DelegateExecution delegateExecution) {
 		LOGGER.info(String.format("activity=%s", delegateExecution.getCurrentActivityName()));
 	}
 }
